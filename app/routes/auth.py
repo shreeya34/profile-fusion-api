c03825a1a22832cae9a29937b03273ab5580f4e5
shreeya34 <1shreeya2@gmail.com>
2025-02-11 20:53:14 +0545

    work on handle username exists

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -37,8 +37,6 @@ async def create_user(user: UserCreate, db: Session = Depends(get_db)):
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Email already registered",
         )
-
-   
         return UserService.create_user(db, user)
 
 @router.get("/exists",tags=['users'])
